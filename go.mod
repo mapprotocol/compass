4d5e24d9415449a670a832e1ba3877d24f097b09
yangdianqing <phpgoc@icloud.com>
2021-07-14 17:46:39 +0800

    use cobra lib to parse command parameters， --password maybe cannot use now.

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/ethereum/go-ethereum v1.10.4
-	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	github.com/spf13/cobra v1.2.1
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
 )
---
+++ libs/cmd/daemon.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/spf13/cobra"
+	"log"
+	"math/rand"
+	"os"
+	"signmap/libs"
+	"signmap/libs/contracts/matic_data"
+	"signmap/libs/contracts/matic_staking"
+	"strings"
+	"time"
+)
+
+var cmdDeemon = &cobra.Command{
+	Use:   "daemon ",
+	Short: "Run signmap deemon",
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		p := flag.String("password", "", "Enter the password on the command line. not recommend. ")
+		flag.Parse()
+		libs.GetKey(*p)
+		if bytes.Compare(matic_data.BindAddress().Bytes(), common.Address{}.Bytes()) == 0 {
+			println("Worker not set！ please set a worker.")
+			os.Exit(1)
+		}
+		matic_data.GetData()
+		rand.Seed(time.Now().UnixNano())
+		libs.WriteLog("starting success!")
+		//signUnit := rand.Intn(24 * 60) //for production
+		var everyNMinute = 1                 // require 60 % everyNMinute == 0 //for test
+		signUnit := rand.Intn(everyNMinute)  //for test
+		log.Println("signUnit = ", signUnit) // for test , production environment does not print
+		c := make(chan bool)
+		go func(cc chan bool) {
+			for {
+				_ = <-cc
+				//nowTime,date := libs.NowTime()         // for production
+				nowUnit, date := libs.NowTimeForTestEveryNMinute(everyNMinute) //for test
+				if nowUnit == 0 {
+					//signUnit = rand.Intn(24 * 60) //for production
+					signUnit = rand.Intn(everyNMinute) //for test
+					log.Println("signUnit = ", signUnit)
+				}
+
+				if nowUnit == signUnit && !strings.HasPrefix(libs.GetLastLineWithSeek(), date) {
+					log.Println(date)
+					// Determine if you have signed it today
+					libs.WriteLog(fmt.Sprintf("%s %d Sign in successfully.", date, nowUnit))
+					//libs.SendTransaction()
+					if matic_staking.DO() {
+						matic_data.GetData()
+					}
+				}
+			}
+		}(c)
+		for {
+			c <- true
+			time.Sleep(time.Minute)
+		}
+	},
+}
