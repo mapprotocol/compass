f408d3ff3ae4919af96396e851fcba659ac1ae77
yangdianqing <phpgoc@icloud.com>
2021-07-14 17:17:14 +0800

    print sign in successful

--- libs/config.go
+++ libs/config.go
@@ -13,8 +13,8 @@ const (
 
 var (
 	SendTransactionValue        = big.NewInt(1000000000000000000)
-	MaticStakingContractAddress = common.HexToAddress("0xD9397A23c06418E5733ECBb434A3008e3CbB7647")
-	MaticDataContractAddress    = common.HexToAddress("0xb4D6d8f5E7f971B8B89d15d813C6d3278A66BcbD")
+	MaticStakingContractAddress = common.HexToAddress("0x821dD65Dbeb1a9F4846ce5E0d74A22869Ef5755d")
+	MaticDataContractAddress    = common.HexToAddress("0x17c6b58499dF2E70882C2a2A8D22F2Decc6e8F98")
 	SendTransactionGasLimit     = uint64(21000)
 	ToAddress                   = common.HexToAddress("0x799E24dC6B48549BbD1Fc9fcCa4d72880d8c7a15")
 	SignLogFile, _              = os.OpenFile(filepath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
