90b4b343a16bb814cc71d8ec3d64c69b9e3303a4
yangdianqing <phpgoc@icloud.com>
2021-08-09 16:17:15 +0800

    info subcommand watch,continuous query info

--- libs/sync_cmd/cmd_info.go
+++ libs/sync_cmd/cmd_info.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spf13/cobra"
 	"signmap/libs"
 	"strconv"
+	"time"
 )
 
 var (
@@ -14,41 +15,74 @@ var (
 		Short: "Get account info.",
 		Args:  cobra.MinimumNArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
-			initClient()
-			relayerBalance := dstInstance.GetRelayerBalance()
-			relayer := dstInstance.GetRelayer()
-			table := simpletable.New()
-			table.Header = &simpletable.Header{
-				Cells: []*simpletable.Cell{
-					{Align: simpletable.AlignCenter, Text: "name"},
-					{Align: simpletable.AlignCenter, Text: "value"},
-				},
+			if len(args) != 0 {
+				err := cmd.Help()
+				if err != nil {
+					return
+				}
+				return
 			}
-			table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
-				{Text: "registered amount"},
-				{Text: libs.WeiToEther(relayerBalance.Register).String()},
-			})
-			table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
-				{Text: "locked amount"},
-				{Text: libs.WeiToEther(relayerBalance.Locked).String()},
-			})
-			table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
-				{Text: "redeemable amount"},
-				{Text: libs.WeiToEther(relayerBalance.Unlocked).String()},
-			})
-			table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
-				{Text: "is registered"},
-				{Text: strconv.FormatBool(relayer.Register)},
-			})
-			table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
-				{Text: "is relayer"},
-				{Text: strconv.FormatBool(relayer.Relayer)},
-			})
-			table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
-				{Text: "current epoch"},
-				{Text: relayer.Epoch.String()},
-			})
-			fmt.Println(table.String())
+			initClient()
+			displayOnce()
+		},
+	}
+	cmdInfoWatch = &cobra.Command{
+		Use:   "watch ",
+		Short: "Get account info every some seconds ,default 5 seconds.",
+		Args:  cobra.MinimumNArgs(0),
+		Run: func(cmd *cobra.Command, args []string) {
+			initClient()
+			var interval = 5
+			if len(args) != 0 {
+				if i, err := strconv.Atoi(args[0]); err == nil {
+					interval = i
+				}
+			}
+			for {
+				displayOnce()
+				time.Sleep(time.Duration(interval) * time.Second)
+			}
 		},
 	}
 )
+
+func cmdInfoFunc() *cobra.Command {
+	cmdInfo.AddCommand(cmdInfoWatch)
+	return cmdInfo
+}
+func displayOnce() {
+	relayerBalance := dstInstance.GetRelayerBalance()
+	relayer := dstInstance.GetRelayer()
+	table := simpletable.New()
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "name"},
+			{Align: simpletable.AlignCenter, Text: "value"},
+		},
+	}
+	table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
+		{Text: "registered amount"},
+		{Text: libs.WeiToEther(relayerBalance.Register).String()},
+	})
+	table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
+		{Text: "locked amount"},
+		{Text: libs.WeiToEther(relayerBalance.Locked).String()},
+	})
+	table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
+		{Text: "redeemable amount"},
+		{Text: libs.WeiToEther(relayerBalance.Unlocked).String()},
+	})
+	table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
+		{Text: "is registered"},
+		{Text: strconv.FormatBool(relayer.Register)},
+	})
+	table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
+		{Text: "is relayer"},
+		{Text: strconv.FormatBool(relayer.Relayer)},
+	})
+	table.Body.Cells = append(table.Body.Cells, []*simpletable.Cell{
+		{Text: "current epoch"},
+		{Text: relayer.Epoch.String()},
+	})
+	fmt.Println(table.String())
+}
