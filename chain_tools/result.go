c6b6196847948cb76acb902fcc7386e655e0028e
yangdianqing <phpgoc@icloud.com>
2021-08-16 11:13:12 +0800

    add WaitForReceipt func, if call need wait for it

--- chain_tools/result.go
+++ chain_tools/result.go
@@ -3,15 +3,16 @@ package chain_tools
 import (
 	"context"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	log "github.com/sirupsen/logrus"
 	"time"
 )
 
-func WaitingForEndPending(conn *ethclient.Client, txHash common.Hash) bool {
+func WaitingForEndPending(conn *ethclient.Client, txHash common.Hash, waitingSeconds int) bool {
 	count := 0
+	time.Sleep(time.Millisecond * 200)
 	for {
-		time.Sleep(time.Millisecond * 200)
 		_, isPending, err := conn.TransactionByHash(context.Background(), txHash)
 		if err != nil {
 			log.Infoln(err)
@@ -21,10 +22,36 @@ func WaitingForEndPending(conn *ethclient.Client, txHash common.Hash) bool {
 		if !isPending {
 			break
 		}
-		if count >= 100 {
+		if count >= waitingSeconds {
 			log.Warnln("Not waiting for the result.")
 			return false
 		}
+		time.Sleep(time.Second)
 	}
 	return true
+}
+func WaitForReceipt(conn *ethclient.Client, txHash common.Hash) bool {
+	onceTime := time.Second
+	for {
+		receipt, err := conn.TransactionReceipt(context.Background(), txHash)
+		if err != nil {
+			log.Warnln("Get receipt error: ", err)
+			time.Sleep(onceTime)
+			continue
+		}
+		switch receipt.Status {
+		case types.ReceiptStatusSuccessful:
+			println("Sign in successfully.")
+			return true
+		case types.ReceiptStatusFailed:
+			log.Warnln("Transaction not completed，unconfirmed.", txHash.String())
+			return false
+		default:
+			//should unreachable
+			log.Warnln("Unknown receipt status: ", txHash.String(), receipt.Status)
+			time.Sleep(onceTime / 2)
+			continue
+		}
+	}
+
 }
