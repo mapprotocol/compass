a73158611e537c36b08eea2496c45851d91ddb04
jsonfelix <wbqlove1234@gmail.com>
2021-08-24 17:42:11 +0800

    Separate testing and formal environments

--- contracts/scriptsmain/deploy_ethereum.js
+++ contracts/scriptsmain/deploy_ethereum.js
@@ -15,13 +15,12 @@ async function main() {
     console.log("EthData address:", eData.address);
 
     const EthStaking = await ethers.getContractFactory("EthereumStaking");
-    //test
-    const eStaking = await EthStaking.deploy(eData.address,"0x07cc6bbe1ea85a39ee3fe359750a553a906fbf4e");
+    const eStaking = await EthStaking.deploy(eData.address,"0x9E976F211daea0D652912AB99b0Dc21a7fD728e4");
     await eStaking.deployed();
     console.log("EthStaking address:", eStaking.address);
 
     await eData.addManager(eStaking.address);
-    await eStaking.addManager("0x228F78fC398DB973B96eD666C92E78753b9466Eb")
+    await eStaking.addManager("0x200aee9ba7040d778922a763ce8a50948d61aff5");
 
 }
 
