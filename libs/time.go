f9311d8590f8b92920b74adf6ca6df39929daff8
yangdianqing <phpgoc@icloud.com>
2021-08-24 16:47:39 +0800

    now time use utc time

--- libs/time.go
+++ libs/time.go
@@ -6,13 +6,11 @@ import (
 )
 
 func NowTime() (int, string) {
-	now := time.Now()
+	now := time.Now().UTC()
+
 	return now.Hour()*60 + now.Minute(), now.Format("20060102")
 }
-func NowTimeForTest() (int, string) {
-	now := time.Now()
-	return now.Minute(), now.Format("2006010215")
-}
+
 func NowTimeForTestEveryNMinute(n int) (int, string) {
 	now := time.Now()
 	return now.Minute() % n, now.Format("2006010215") + fmt.Sprint(now.Minute()/n)
