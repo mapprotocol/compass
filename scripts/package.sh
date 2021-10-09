374ba41371ef5c7acbd9bb5185784430f9c86c0d
Jae Liu <401811826@qq.com>
2021-10-09 17:10:47 +0800

    chore:to use tar for compressing binary files with darwin and linux releases

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,14 +6,15 @@ popd >/dev/null
 
 cd "$cmdDir"/../target
 rm -f *.zip
+rm -f *.tar.gz
 mv signmap_darwin_amd64.exe signmap
-zip signmap_darwin_amd64.zip signmap
+tar czvf signmap_darwin_amd64.tar.gz signmap
 
 mv signmap_darwin_arm64.exe signmap
-zip signmap_darwin_arm64.zip signmap
+tar czvf signmap_darwin_arm64.tar.gz signmap
 
 mv signmap_linux_amd64.exe signmap
-zip signmap_linux_amd64.zip signmap
+tar czvf signmap_linux_amd64.tar.gz signmap
 
 mv signmap_windows_amd64.exe signmap.exe
 printf "signmap\\r\\npause" > signmap.bat
