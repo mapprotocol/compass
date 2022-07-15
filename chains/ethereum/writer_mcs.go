6392cc30862b8cabc1be272d9ab53dc53c36921d
lbtsm <1425930815@qq.com>
2022-07-15 17:35:01 +0800

    map2eth(block、log)

---
+++ chains/ethereum/writer_mcs.go
@@ -0,0 +1,92 @@
+package ethereum
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/mapprotocol/compass/mapprotocol"
+	"github.com/mapprotocol/compass/msg"
+	"github.com/pkg/errors"
+)
+
+// exeSwapMsg executes swap msg, and send tx to the destination blockchain
+func (w *writer) exeSwapMsg(m msg.Message) bool {
+	return w.callContractWithMsg(w.cfg.bridgeContract, m)
+}
+
+// callContractWithMsg call contract using address and function signature with message info
+func (w *writer) callContractWithMsg(addr common.Address, m msg.Message) bool {
+	for i := 0; i < TxRetryLimit; i++ {
+		select {
+		case <-w.stop:
+			return false
+		default:
+			err := w.conn.LockAndUpdateOpts()
+			if err != nil {
+				w.log.Error("Failed to update nonce", "err", err)
+				return false
+			}
+			// These store the gas limit and price before a transaction is sent for logging in case of a failure
+			// This is necessary as tx will be nil in the case of an error when sending VoteProposal()
+			gasLimit := w.conn.Opts().GasLimit
+			gasPrice := w.conn.Opts().GasPrice
+			err = w.call(&addr, m.Payload[0].([]byte), mapprotocol.ABILightNode, mapprotocol.MethodVerifyProofData)
+			w.conn.UnlockOpts()
+
+			if err == nil {
+				// message successfully handled
+				w.log.Info("Submitted cross tx execution", "src", m.Source, "dst", m.Destination, "nonce", m.DepositNonce)
+				m.DoneCh <- struct{}{}
+				return true
+			} else if err.Error() == ErrNonceTooLow.Error() || err.Error() == ErrTxUnderpriced.Error() {
+				w.log.Error("Nonce too low, will retry")
+				time.Sleep(TxRetryInterval)
+			} else {
+				w.log.Warn("Execution failed, tx may already be complete", "gasLimit", gasLimit, "gasPrice", gasPrice, "err", err)
+				time.Sleep(TxRetryInterval)
+			}
+		}
+	}
+	w.log.Error("Submission of Execute transaction failed", "source", m.Source, "dest", m.Destination, "depositNonce", m.DepositNonce)
+	w.sysErr <- ErrFatalTx
+	return false
+}
+
+func (w *writer) call(toAddress *common.Address, input []byte, useAbi abi.ABI, method string) error {
+	from := w.conn.Keypair().CommonAddress()
+	output, err := w.conn.Client().CallContract(context.Background(),
+		ethereum.CallMsg{
+			From: from,
+			To:   toAddress,
+			Data: input,
+		},
+		nil,
+	)
+	if err != nil {
+		w.log.Error("", err.Error())
+	}
+
+	resp, err := useAbi.Methods[method].Outputs.Unpack(output)
+	if err != nil {
+		w.log.Error("proof call failed ", "err", err.Error())
+	}
+	ret := struct {
+		Success  bool
+		Message  string
+		LogsHash []byte
+	}{}
+
+	err = useAbi.Methods[method].Outputs.Copy(&ret, resp)
+	if err != nil {
+		return errors.Wrap(err, "proof copy failed")
+	}
+	if !ret.Success {
+		return fmt.Errorf("verify proof failed, message is (%s)", ret.Message)
+	}
+
+	return nil
+}
