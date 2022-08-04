3c37ba8b2020b8c8f55f0a6d54fa08fa29a4b3e5
lbtsm <1425930815@qq.com>
2022-08-04 17:08:04 +0800

    add near verify

--- internal/near/protocol.go
+++ internal/near/protocol.go
@@ -1,6 +1,10 @@
 package near
 
-import "github.com/mapprotocol/near-api-go/pkg/types"
+import (
+	"math/big"
+
+	"github.com/mapprotocol/near-api-go/pkg/types"
+)
 
 var (
 	NewFunctionCallGas types.Gas = 30 * 10000000000000
@@ -11,4 +15,15 @@ type Result struct {
 	BlockHeight int           `json:"block_height"`
 	Logs        []interface{} `json:"logs"`
 	Result      []byte        `json:"result"`
+}
+
+type TransferOut struct {
+	Token        string  `json:"token"`
+	From         string  `json:"from"`
+	OrderId      []byte  `json:"order_id"`
+	FromChain    big.Int `json:"from_chain"`
+	ToChain      big.Int `json:"to_chain"`
+	To           string  `json:"to"`
+	Amount       big.Int `json:"amount"`
+	ToChainToken string  `json:"to_chain_token"`
 }
