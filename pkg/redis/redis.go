25b75fcae85fd29d6409776f587ab8c3c17f47f3
lbtsm <1425930815@qq.com>
2022-08-10 17:35:08 +0800

    modify near verify paoram

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -10,12 +10,18 @@ var (
 )
 
 func init() {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "46.137.199.126:6379",
-		Password: "F6U3gV0L6Xwyw1Ko", // 密码
-		DB:       0,                  // 数据库
-		PoolSize: 20,                 // 连接池大小
-	})
+	ops, err := redis.ParseURL("redis://default:@46.137.199.126:6379/0")
+	if err != nil {
+		panic(err)
+	}
+	//rdb := redis.NewClient(&redis.Options{
+	//	Addr:     "46.137.199.126:6379",
+	//	Password: "F6U3gV0L6Xwyw1Ko", // 密码
+	//	DB:       0,                  // 数据库
+	//	PoolSize: 20,                 // 连接池大小
+	//})
+	ops.PoolSize = 20
+	rdb := redis.NewClient(ops)
 
 	redisClient = rdb
 }
