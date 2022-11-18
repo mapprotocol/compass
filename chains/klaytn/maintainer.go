b71b96ff8ba3f79c8f242102f4e5992a6e6600b3
lbtsm <1425930815@qq.com>
2022-11-18 17:44:39 +0800

    klayan

---
+++ chains/klaytn/maintainer.go
@@ -0,0 +1,197 @@
+package klaytn
+
+import (
+	"context"
+	"github.com/mapprotocol/compass/internal/chain"
+	"math/big"
+	"time"
+
+	"github.com/mapprotocol/compass/internal/bsc"
+
+	"github.com/mapprotocol/compass/internal/constant"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/mapprotocol/compass/mapprotocol"
+	"github.com/mapprotocol/compass/msg"
+	"github.com/pkg/errors"
+)
+
+type Maintainer struct {
+	*chain.CommonSync
+	syncedHeight *big.Int
+}
+
+func NewMaintainer(cs *chain.CommonSync) *Maintainer {
+	return &Maintainer{
+		CommonSync:   cs,
+		syncedHeight: new(big.Int),
+	}
+}
+
+func (m *Maintainer) Sync() error {
+	m.Log.Debug("Starting listener...")
+	go func() {
+		err := m.sync()
+		if err != nil {
+			m.Log.Error("Polling blocks failed", "err", err)
+		}
+	}()
+
+	return nil
+}
+
+// sync function of Maintainer will poll for the latest block and proceed to parse the associated events as it sees new blocks.
+// Polling begins at the block defined in `m.Cfg.startBlock`. Failed attempts to fetch the latest block or parse
+// a block will be retried up to BlockRetryLimit times before continuing to the next block.
+func (m Maintainer) sync() error {
+	var currentBlock = m.Cfg.StartBlock
+	m.Log.Info("Polling Blocks...", "block", currentBlock)
+
+	if m.Cfg.SyncToMap {
+		// check whether needs quick listen
+		//syncedHeight, err := mapprotocol.Get2MapByLight()
+		syncedHeight, err := mapprotocol.Get2MapHeight(m.Cfg.Id)
+		if err != nil {
+			m.Log.Error("Get synced Height failed", "err", err)
+			return err
+		}
+
+		m.Log.Info("Check Sync Status...", "synced", syncedHeight)
+		m.syncedHeight = syncedHeight
+
+		if syncedHeight.Cmp(currentBlock) != 0 {
+			m.Log.Info("SyncedHeight is higher or lower than currentHeight, so let currentHeight = syncedHeight",
+				"syncedHeight", syncedHeight, "currentBlock", currentBlock)
+			currentBlock.Add(syncedHeight, new(big.Int).SetInt64(mapprotocol.HeaderCountOfBsc))
+		}
+	}
+
+	var retry = constant.BlockRetryLimit
+	for {
+		select {
+		case <-m.Stop:
+			return errors.New("polling terminated")
+		default:
+			// No more retries, goto next block
+			if retry == 0 {
+				m.Log.Error("Polling failed, retries exceeded")
+				m.SysErr <- constant.ErrFatalPolling
+				return nil
+			}
+
+			latestBlock, err := m.Conn.LatestBlock()
+			if err != nil {
+				m.Log.Error("Unable to get latest block", "block", currentBlock, "err", err)
+				retry--
+				time.Sleep(constant.BlockRetryInterval)
+				continue
+			}
+
+			if m.Metrics != nil {
+				m.Metrics.LatestKnownBlock.Set(float64(latestBlock.Int64()))
+			}
+
+			// Sleep if the difference is less than BlockDelay; (latest - current) < BlockDelay
+			if big.NewInt(0).Sub(latestBlock, currentBlock).Cmp(m.BlockConfirmations) == -1 {
+				m.Log.Debug("Block not ready, will retry", "current", currentBlock, "latest", latestBlock)
+				time.Sleep(constant.BlockRetryInterval)
+				continue
+			}
+
+			if m.Cfg.SyncToMap && currentBlock.Cmp(m.syncedHeight) == 1 {
+				// Sync headers to Map
+				err = m.syncHeaderToMap(currentBlock)
+				if err != nil {
+					m.Log.Error("Failed to listen header for block", "block", currentBlock, "err", err)
+					retry--
+					time.Sleep(constant.BlockRetryInterval)
+					continue
+				}
+			}
+
+			// Write to block store. Not a critical operation, no need to retry
+			err = m.BlockStore.StoreBlock(currentBlock)
+			if err != nil {
+				m.Log.Error("Failed to write latest block to blockstore", "block", currentBlock, "err", err)
+			}
+
+			if m.Metrics != nil {
+				m.Metrics.BlocksProcessed.Inc()
+				m.Metrics.LatestProcessedBlock.Set(float64(latestBlock.Int64()))
+			}
+
+			m.LatestBlock.Height = big.NewInt(0).Set(latestBlock)
+			m.LatestBlock.LastUpdated = time.Now()
+
+			currentBlock.Add(currentBlock, big.NewInt(1))
+			retry = constant.BlockRetryLimit
+		}
+	}
+}
+
+// syncHeaderToMap listen header from current chain to Map chain
+func (m *Maintainer) syncHeaderToMap(latestBlock *big.Int) error {
+	remainder := big.NewInt(0).Mod(new(big.Int).Sub(latestBlock, new(big.Int).SetInt64(mapprotocol.HeaderCountOfBsc-1)),
+		big.NewInt(mapprotocol.EpochOfBsc))
+	if remainder.Cmp(mapprotocol.Big0) != 0 {
+		return nil
+	}
+	chainBlcNum, err := m.Conn.Client().BlockNumber(context.Background())
+	if err != nil {
+		return errors.Wrap(err, "get latest chainBlcNum failed")
+	}
+	// latestBlock must less than blockNumber of chain online，otherwise time.sleep
+	difference := new(big.Int).Sub(latestBlock, new(big.Int).SetUint64(chainBlcNum))
+	if difference.Int64() > 0 {
+		m.Log.Info("chain online blockNumber less than local latestBlock, waiting...", "chainBlcNum", chainBlcNum,
+			"localBlock", latestBlock, "waiting", difference.Int64())
+		time.Sleep(constant.BlockRetryInterval * time.Duration(difference.Int64()))
+	}
+	m.Log.Info("find sync block", "current height", latestBlock)
+	syncedHeight, err := mapprotocol.Get2MapHeight(m.Cfg.Id)
+	if err != nil {
+		m.Log.Error("Get current synced Height failed", "err", err)
+		return err
+	}
+	if latestBlock.Cmp(syncedHeight) <= 0 {
+		m.Log.Info("CurrentBlock less than synchronized headerHeight", "synced height", syncedHeight,
+			"current height", latestBlock)
+		return nil
+	}
+
+	headers := make([]types.Header, mapprotocol.HeaderCountOfBsc)
+	for i := 0; i < mapprotocol.HeaderCountOfBsc; i++ {
+		headerHeight := new(big.Int).Sub(latestBlock, new(big.Int).SetInt64(int64(i)))
+		header, err := m.Conn.Client().HeaderByNumber(context.Background(), headerHeight)
+		if err != nil {
+			return err
+		}
+		headers[mapprotocol.HeaderCountOfBsc-i-1] = *header
+	}
+
+	params := make([]bsc.Header, 0, len(headers))
+	for _, h := range headers {
+		params = append(params, bsc.ConvertHeader(h))
+	}
+	input, err := mapprotocol.Bsc.Methods[mapprotocol.MethodOfGetHeadersBytes].Inputs.Pack(params)
+	if err != nil {
+		m.Log.Error("failed to abi pack", "err", err)
+		return err
+	}
+
+	id := big.NewInt(0).SetUint64(uint64(m.Cfg.Id))
+	msgpayload := []interface{}{id, input}
+	message := msg.NewSyncToMap(m.Cfg.Id, m.Cfg.MapChainID, msgpayload, m.MsgCh)
+
+	err = m.Router.Send(message)
+	if err != nil {
+		m.Log.Error("subscription error: failed to route message", "err", err)
+		return err
+	}
+
+	err = m.WaitUntilMsgHandled(1)
+	if err != nil {
+		return err
+	}
+	return nil
+}
