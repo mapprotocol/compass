5667d7f388c36d92e9dea4cf6482007f8896cdc4
lbtsm <1425930815@qq.com>
2022-12-07 16:02:12 +0800

    eth2

---
+++ chains/eth2/chain.go
@@ -0,0 +1,141 @@
+package eth2
+
+import (
+	"github.com/ChainSafe/chainbridge-utils/crypto/secp256k1"
+	metrics "github.com/ChainSafe/chainbridge-utils/metrics/types"
+	"github.com/ChainSafe/log15"
+	"github.com/mapprotocol/compass/chains"
+	"github.com/mapprotocol/compass/core"
+	"github.com/mapprotocol/compass/internal/chain"
+	w "github.com/mapprotocol/compass/internal/writer"
+	"github.com/mapprotocol/compass/keystore"
+	"github.com/mapprotocol/compass/mapprotocol"
+)
+
+var _ core.Chain = new(Chain)
+
+type Chain struct {
+	cfg    *core.ChainConfig // The config of the chain
+	conn   chain.KConnection // The chains connection
+	writer *w.Writer         // The writer of the chain
+	stop   chan<- int
+	listen chains.Listener // The listener of this chain
+}
+
+func InitializeChain(chainCfg *core.ChainConfig, logger log15.Logger, sysErr chan<- error, m *metrics.ChainMetrics,
+	role mapprotocol.Role) (*Chain, error) {
+	cfg, err := chain.ParseConfig(chainCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	kpI, err := keystore.KeypairFromAddress(cfg.From, keystore.EthChain, cfg.KeystorePath, chainCfg.Insecure)
+	if err != nil {
+		return nil, err
+	}
+	kp, _ := kpI.(*secp256k1.Keypair)
+
+	bs, err := chain.SetupBlockStore(cfg, kp, role)
+	if err != nil {
+		return nil, err
+	}
+
+	stop := make(chan int)
+	conn := connection.NewConnection(cfg.Endpoint, cfg.Http, kp, logger, cfg.GasLimit, cfg.MaxGasPrice,
+		cfg.GasMultiplier, cfg.EgsApiKey, cfg.EgsSpeed)
+	err = conn.Connect()
+	if err != nil {
+		return nil, err
+	}
+
+	if chainCfg.LatestBlock {
+		curr, err := conn.LatestBlock()
+		if err != nil {
+			return nil, err
+		}
+		cfg.StartBlock = curr
+	}
+
+	// simplified a little bit
+	var listen chains.Listener
+	cs := chain.NewCommonSync(conn, cfg, logger, stop, sysErr, m, bs)
+	if role == mapprotocol.RoleOfMaintainer {
+		//fn := mapprotocol.Map2EthHeight(cfg.From, cfg.LightNode, conn.Client())
+		//height, err := fn()
+		//if err != nil {
+		//	return nil, errors.Wrap(err, "klaytn get init headerHeight failed")
+		//}
+		//logger.Info("map2Klaytn Current situation", "height", height, "lightNode", cfg.LightNode)
+		//mapprotocol.SyncOtherMap[cfg.Id] = height
+		//mapprotocol.Map2OtherHeight[cfg.Id] = fn
+		listen = NewMaintainer(cs, conn.KClient())
+	} else if role == mapprotocol.RoleOfMessenger {
+		err = conn.EnsureHasBytecode(cfg.McsContract)
+		if err != nil {
+			return nil, err
+		}
+		//// verify range
+		//fn := mapprotocol.Map2EthVerifyRange(cfg.From, cfg.LightNode, conn.Client())
+		//left, right, err := fn()
+		//if err != nil {
+		//	return nil, errors.Wrap(err, "kalytn get init verifyHeight failed")
+		//}
+		//logger.Info("Map2Klaytn Current verify range", "left", left, "right", right, "lightNode", cfg.LightNode)
+		//mapprotocol.Map2OtherVerifyRange[cfg.Id] = fn
+		listen = NewMessenger(cs, conn.KClient())
+	}
+	wri := w.New(conn, cfg, logger, stop, sysErr, m)
+
+	return &Chain{
+		cfg:    chainCfg,
+		conn:   conn,
+		writer: wri,
+		stop:   stop,
+		listen: listen,
+	}, nil
+}
+
+func (c *Chain) SetRouter(r *core.Router) {
+	r.Listen(c.cfg.Id, c.writer)
+	c.listen.SetRouter(r)
+}
+
+func (c *Chain) Start() error {
+	err := c.listen.Sync()
+	if err != nil {
+		return err
+	}
+
+	log.Debug("Successfully started chain")
+	return nil
+}
+
+func (c *Chain) Id() msg.ChainId {
+	return c.cfg.Id
+}
+
+func (c *Chain) Name() string {
+	return c.cfg.Name
+}
+
+func (c *Chain) LatestBlock() metrics.LatestBlock {
+	return c.listen.GetLatestBlock()
+}
+
+// Stop signals to any running routines to exit
+func (c *Chain) Stop() {
+	close(c.stop)
+	if c.conn != nil {
+		c.conn.Close()
+	}
+}
+
+// EthClient return EthClient for global map connection
+func (c *Chain) EthClient() *ethclient.Client {
+	return c.conn.Client()
+}
+
+// Conn return Connection interface for relayer register
+func (c *Chain) Conn() chain.Connection {
+	return c.conn
+}
--- internal/eth2/resp.go
+++ internal/eth2/resp.go
@@ -1,8 +1,18 @@
 package eth2
 
-type BeaconHeaders struct {
-	Data                Data `json:"data"`
-	ExecutionOptimistic bool `json:"execution_optimistic"`
+type BeaconHeadersResp struct {
+	Data                BeaconHeadersData `json:"data"`
+	ExecutionOptimistic bool              `json:"execution_optimistic"`
+}
+
+type FinalityUpdateResp struct {
+	Data FinalityUpdateData `json:"data"`
+}
+
+type BlocksResp struct {
+	Data                BlocksData `json:"data"`
+	Version             string     `json:"version"`
+	ExecutionOptimistic bool       `json:"execution_optimistic"`
 }
 
 type Message struct {
@@ -18,8 +28,91 @@ type Header struct {
 	Signature string  `json:"signature"`
 }
 
-type Data struct {
+type BeaconHeadersData struct {
 	Root      string `json:"root"`
 	Canonical bool   `json:"canonical"`
 	Header    Header `json:"header"`
+}
+
+type AttestedHeader struct {
+	Slot          string `json:"slot"`
+	ProposerIndex string `json:"proposer_index"`
+	ParentRoot    string `json:"parent_root"`
+	StateRoot     string `json:"state_root"`
+	BodyRoot      string `json:"body_root"`
+}
+
+type FinalizedHeader struct {
+	Slot          string `json:"slot"`
+	ProposerIndex string `json:"proposer_index"`
+	ParentRoot    string `json:"parent_root"`
+	StateRoot     string `json:"state_root"`
+	BodyRoot      string `json:"body_root"`
+}
+
+type SyncAggregate struct {
+	SyncCommitteeBits      string `json:"sync_committee_bits"`
+	SyncCommitteeSignature string `json:"sync_committee_signature"`
+}
+
+type FinalityUpdateData struct {
+	AttestedHeader  AttestedHeader  `json:"attested_header"`
+	FinalizedHeader FinalizedHeader `json:"finalized_header"`
+	FinalityBranch  []string        `json:"finality_branch"`
+	SyncAggregate   SyncAggregate   `json:"sync_aggregate"`
+	SignatureSlot   string          `json:"signature_slot"`
+}
+
+type Eth1Data struct {
+	DepositRoot  string `json:"deposit_root"`
+	DepositCount string `json:"deposit_count"`
+	BlockHash    string `json:"block_hash"`
+}
+
+type Source struct {
+	Epoch string `json:"epoch"`
+	Root  string `json:"root"`
+}
+
+type Target struct {
+	Epoch string `json:"epoch"`
+	Root  string `json:"root"`
+}
+
+type BlocksData struct {
+	Slot            string `json:"slot"`
+	Index           string `json:"index"`
+	BeaconBlockRoot string `json:"beacon_block_root"`
+	Source          Source `json:"source"`
+	Target          Target `json:"target"`
+}
+
+type Attestations struct {
+	AggregationBits string `json:"aggregation_bits"`
+	Data            Data   `json:"data"`
+	Signature       string `json:"signature"`
+}
+
+type Body struct {
+	RandaoReveal      string         `json:"randao_reveal"`
+	Eth1Data          Eth1Data       `json:"eth1_data"`
+	Graffiti          string         `json:"graffiti"`
+	ProposerSlashings []interface{}  `json:"proposer_slashings"`
+	AttesterSlashings []interface{}  `json:"attester_slashings"`
+	Attestations      []Attestations `json:"attestations"`
+	Deposits          []interface{}  `json:"deposits"`
+	VoluntaryExits    []interface{}  `json:"voluntary_exits"`
+}
+
+type BlocksMessage struct {
+	Slot          string `json:"slot"`
+	ProposerIndex string `json:"proposer_index"`
+	ParentRoot    string `json:"parent_root"`
+	StateRoot     string `json:"state_root"`
+	Body          Body   `json:"body"`
+}
+
+type Data struct {
+	Message   BlocksMessage `json:"message"`
+	Signature string        `json:"signature"`
 }
