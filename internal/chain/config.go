914aa264c00e108a637512d4a53638f9202cf20c
lbtsm <1425930815@qq.com>
2022-12-18 15:53:56 +0800

    default

--- internal/chain/config.go
+++ internal/chain/config.go
@@ -20,7 +20,7 @@ import (
 const (
 	DefaultGasLimit           = 6721975
 	DefaultGasPrice           = 20000000000
-	DefaultBlockConfirmations = 10
+	DefaultBlockConfirmations = 20
 	DefaultGasMultiplier      = 1
 )
 
