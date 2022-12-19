a3db67d181f30d87978170cf70c957f0990a19ad
lbtsm <1425930815@qq.com>
2022-12-19 16:00:36 +0800

    eth2

--- connections/eth2/connection.go
+++ connections/eth2/connection.go
@@ -10,18 +10,19 @@ import (
 )
 
 type Connection struct {
-	endpoint string
+	endpoint, eth2Endpoint string
 	*ethereum.Connection
 	eth2Conn *eth2.Client
 }
 
 // NewConnection returns an uninitialized connection, must call Connection.Connect() before using.
-func NewConnection(endpoint string, http bool, kp *secp256k1.Keypair, log log15.Logger, gasLimit, gasPrice *big.Int,
+func NewConnection(endpoint, eth2Endpoint string, http bool, kp *secp256k1.Keypair, log log15.Logger, gasLimit, gasPrice *big.Int,
 	gasMultiplier *big.Float, gsnApiKey, gsnSpeed string) chain.Eth2Connection {
 	conn := ethereum.NewConnection(endpoint, http, kp, log, gasLimit, gasPrice, gasMultiplier, gsnApiKey, gsnSpeed)
 	return &Connection{
-		Connection: conn,
-		endpoint:   endpoint,
+		Connection:   conn,
+		endpoint:     endpoint,
+		eth2Endpoint: eth2Endpoint,
 	}
 }
 
@@ -34,7 +35,8 @@ func (c *Connection) Connect() error {
 	if err := c.Connection.Connect(); err != nil {
 		return err
 	}
-	client, err := eth2.DialHttp(c.endpoint)
+
+	client, err := eth2.DialHttp(c.eth2Endpoint)
 	if err != nil {
 		return err
 	}
