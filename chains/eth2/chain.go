c398889ae828fc3017e1bff53a8d46710226a2a1
lbtsm <1425930815@qq.com>
2023-02-02 16:56:19 +0800

    rm import

--- chains/eth2/chain.go
+++ chains/eth2/chain.go
@@ -1,7 +1,6 @@
 package eth2
 
 import (
-	"fmt"
 	"github.com/ChainSafe/chainbridge-utils/crypto/secp256k1"
 	metrics "github.com/ChainSafe/chainbridge-utils/metrics/types"
 	"github.com/ChainSafe/log15"
