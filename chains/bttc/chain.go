f9ef1da2c30a4f19374a69655a071c1e5f30b156
lbtsm <1425930815@qq.com>
2023-09-28 20:40:07 +0800

    bttc

---
+++ chains/bttc/chain.go
@@ -0,0 +1,153 @@
+package bttc
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+
+	"github.com/mapprotocol/compass/internal/bttc"
+
+	metrics "github.com/ChainSafe/chainbridge-utils/metrics/types"
+	"github.com/ChainSafe/log15"
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	connection "github.com/mapprotocol/compass/connections/ethereum"
+	"github.com/mapprotocol/compass/core"
+	"github.com/mapprotocol/compass/internal/chain"
+	"github.com/mapprotocol/compass/internal/matic"
+	"github.com/mapprotocol/compass/internal/tx"
+	"github.com/mapprotocol/compass/mapprotocol"
+	"github.com/mapprotocol/compass/msg"
+)
+
+func NewChain(chainCfg *core.ChainConfig, logger log15.Logger, sysErr chan<- error, m *metrics.ChainMetrics,
+	role mapprotocol.Role) (core.Chain, error) {
+	return chain.New(chainCfg, logger, sysErr, m, role, connection.NewConnection,
+		chain.OptOfSync2Map(syncHeaderToMap),
+		chain.OptOfInitHeight(mapprotocol.HeaderOneCount),
+		chain.OptOfMos(mosHandler),
+	)
+}
+
+func syncHeaderToMap(m *chain.Maintainer, latestBlock *big.Int) error {
+	remainder := big.NewInt(0).Mod(new(big.Int).Sub(latestBlock, big.NewInt(mapprotocol.HeaderLenOfBttc)), big.NewInt(mapprotocol.HeaderCountOfBttc))
+	if remainder.Cmp(mapprotocol.Big0) != 0 {
+		return nil
+	}
+	//syncedHeight, err := mapprotocol.Get2MapHeight(m.Cfg.Id)
+	syncedHeight, err := mapprotocol.Get2MapByLight()
+	if err != nil {
+		m.Log.Error("Get current synced Height failed", "err", err)
+		return err
+	}
+	if latestBlock.Cmp(syncedHeight) <= 0 {
+		m.Log.Info("CurrentBlock less than synchronized headerHeight", "synced height", syncedHeight,
+			"current height", latestBlock)
+		return nil
+	}
+
+	m.Log.Info("Find sync block", "current height", latestBlock)
+	startBlock := new(big.Int).Sub(latestBlock, new(big.Int).SetInt64(mapprotocol.HeaderLenOfBttc+1))
+	headers := make([]*types.Header, mapprotocol.HeaderLenOfBttc)
+	for i := 0; i < int(mapprotocol.HeaderLenOfBttc); i++ {
+		headerHeight := new(big.Int).Add(startBlock, new(big.Int).SetInt64(int64(i)))
+		header, err := m.Conn.Client().HeaderByNumber(context.Background(), headerHeight)
+		if err != nil {
+			return err
+		}
+		headers[i] = header
+	}
+
+	mHeaders := make([]bttc.BlockHeader, 0, len(headers))
+	for _, h := range headers {
+		mHeaders = append(mHeaders, bttc.ConvertHeader(h))
+	}
+
+	input, err := mapprotocol.Bttc.Methods[mapprotocol.MethodOfGetHeadersBytes].Inputs.Pack(mHeaders)
+	if err != nil {
+		m.Log.Error("Failed to abi pack", "err", err)
+		return err
+	}
+
+	id := big.NewInt(0).SetUint64(uint64(m.Cfg.Id))
+	msgpayload := []interface{}{id, input}
+	message := msg.NewSyncToMap(m.Cfg.Id, m.Cfg.MapChainID, msgpayload, m.MsgCh)
+
+	err = m.Router.Send(message)
+	if err != nil {
+		m.Log.Error("Subscription error: failed to route message", "err", err)
+		return err
+	}
+
+	err = m.WaitUntilMsgHandled(1)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func mosHandler(m *chain.Messenger, latestBlock *big.Int) (int, error) {
+	count := 0
+	for idx, addr := range m.Cfg.McsContract {
+		query := m.BuildQuery(addr, m.Cfg.Events, latestBlock, latestBlock)
+		// querying for logs
+		logs, err := m.Conn.Client().FilterLogs(context.Background(), query)
+		if err != nil {
+			return 0, fmt.Errorf("unable to Filter Logs: %w", err)
+		}
+
+		m.Log.Debug("event", "latestBlock ", latestBlock, " logs ", len(logs))
+		// read through the log events and handle their deposit event if handler is recognized
+		for _, log := range logs {
+			// evm event to msg
+			var message msg.Message
+			// getOrderId
+			orderId := log.Data[:32]
+			if m.Cfg.SyncToMap {
+				method := m.GetMethod(log.Topics[0])
+				// when syncToMap we need to assemble a tx proof
+				txsHash, err := tx.GetTxsHashByBlockNumber(m.Conn.Client(), latestBlock)
+				if err != nil {
+					return 0, fmt.Errorf("unable to get tx hashes Logs: %w", err)
+				}
+				receipts, err := tx.GetReceiptsByTxsHash(m.Conn.Client(), txsHash)
+				if err != nil {
+					return 0, fmt.Errorf("unable to get receipts hashes Logs: %w", err)
+				}
+
+				headers := make([]*types.Header, mapprotocol.HeaderLenOfBttc)
+				for i := 0; i < int(mapprotocol.HeaderLenOfBttc); i++ {
+					headerHeight := new(big.Int).Add(latestBlock, new(big.Int).SetInt64(int64(i)))
+					tmp, err := m.Conn.Client().HeaderByNumber(context.Background(), headerHeight)
+					if err != nil {
+						return 0, fmt.Errorf("getHeader failed, err is %v", err)
+					}
+					headers[i] = tmp
+				}
+
+				mHeaders := make([]matic.BlockHeader, 0, len(headers))
+				for _, h := range headers {
+					mHeaders = append(mHeaders, matic.ConvertHeader(h))
+				}
+
+				payload, err := matic.AssembleProof(mHeaders, log, m.Cfg.Id, receipts, method)
+				if err != nil {
+					return 0, fmt.Errorf("unable to Parse Log: %w", err)
+				}
+
+				msgPayload := []interface{}{payload, orderId, latestBlock.Uint64(), log.TxHash}
+				message = msg.NewSwapWithProof(m.Cfg.Id, m.Cfg.MapChainID, msgPayload, m.MsgCh)
+				message.Idx = idx
+
+				m.Log.Info("Event found", "BlockNumber", log.BlockNumber, "txHash", log.TxHash, "logIdx", log.Index, "orderId", ethcommon.Bytes2Hex(orderId))
+				err = m.Router.Send(message)
+				if err != nil {
+					m.Log.Error("subscription error: failed to route message", "err", err)
+				}
+				count++
+			}
+		}
+	}
+
+	return count, nil
+}
