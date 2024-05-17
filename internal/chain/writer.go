4f0fec8321e2913f601da49065836d6c4e2d3295
lbtsm <1425930815@qq.com>
2024-05-17 11:48:37 +0800

    v3

--- internal/chain/writer.go
+++ internal/chain/writer.go
@@ -81,6 +81,7 @@ func (w *Writer) sendTx(toAddress *common.Address, value *big.Int, input []byte)
 		return nil, err
 	}
 
+	//gasLimit = 5000000000
 	gasTipCap := w.conn.Opts().GasTipCap
 	gasFeeCap := w.conn.Opts().GasFeeCap
 	if w.cfg.LimitMultiplier > 1 {
--- internal/mapo/callcontract.go
+++ internal/mapo/callcontract.go
@@ -8,13 +8,15 @@ import (
 	"context"
 	"encoding/binary"
 	"encoding/json"
+	"fmt"
+	"math/big"
+	"strings"
+
 	"github.com/mapprotocol/compass/internal/arb"
 	"github.com/mapprotocol/compass/internal/constant"
 	"github.com/mapprotocol/compass/internal/op"
 	"github.com/mapprotocol/compass/internal/scroll"
 	"github.com/mapprotocol/compass/pkg/util"
-	"math/big"
-	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -48,7 +50,7 @@ func AssembleEthProof(conn *ethclient.Client, log *types.Log, receipts []*types.
 
 		var key []byte
 		key = rlp.AppendUint64(key[:0], uint64(log.TxIndex))
-		pack, err = proof.Oracle(log.BlockNumber, receipt, key, prf, fId, method, log.Index, mapprotocol.ProofAbi)
+		pack, err = proof.Oracle(log.BlockNumber, receipt, key, prf, fId, method, log.TxIndex, mapprotocol.ProofAbi)
 	}
 
 	if err != nil {
@@ -147,10 +149,11 @@ func AssembleMapProof(cli *ethclient.Client, log *types.Log, receipts []*types.R
 			},
 		}
 
+		fmt.Println("log.Index, ------------------------------ ", log.Index, "-------- ", log.TxIndex)
 		var payloads []byte
 		switch proofType {
 		case constant.ProofTypeOfOrigin:
-			payloads, err = proof.V3Pack(fId, method, mapprotocol.Map2Other, log.Index, rp)
+			payloads, err = proof.V3Pack(fId, method, mapprotocol.Map2Other, log.TxIndex, rp)
 		case constant.ProofTypeOfZk:
 			zkProof, err := mapprotocol.GetZkProof(zkUrl, fId, header.Number.Uint64())
 			if err != nil {
@@ -170,7 +173,7 @@ func AssembleMapProof(cli *ethclient.Client, log *types.Log, receipts []*types.R
 
 				payloads, err = proof.Pack(fId, method, mapprotocol.OracleAbi, pd)
 			} else {
-				payloads, err = proof.Oracle(header.Number.Uint64(), receipt, key, prf, fId, method, log.Index, mapprotocol.ProofAbi)
+				payloads, err = proof.Oracle(header.Number.Uint64(), receipt, key, prf, fId, method, log.TxIndex, mapprotocol.ProofAbi)
 			}
 		}
 
