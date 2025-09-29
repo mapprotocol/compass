f56e10980ab5700fd522696eebe2be2007808b9e
lbtsm <able@gamil.com>
2025-09-29 19:51:31 +0800

    feat: client add timeout

--- internal/client/client.go
+++ internal/client/client.go
@@ -4,12 +4,23 @@ import (
 	"bytes"
 	"io"
 	"net/http"
+	"time"
 
 	log "github.com/ChainSafe/log15"
 )
 
+var (
+	cli = http.Client{
+		Timeout: 30 * time.Second,
+	}
+)
+
 func JsonPost(url string, data []byte) ([]byte, error) {
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+	start := time.Now()
+	defer func() {
+		log.Info("JsonPost", "url", url, "duration", time.Since(start))
+	}()
+	resp, err := cli.Post(url, "application/json", bytes.NewBuffer(data))
 	if err != nil {
 		log.Error("JsonPost request error", "url", url, "error", err)
 		return nil, err
@@ -24,7 +35,11 @@ func JsonPost(url string, data []byte) ([]byte, error) {
 }
 
 func JsonGet(url string) ([]byte, error) {
-	resp, err := http.Get(url)
+	start := time.Now()
+	defer func() {
+		log.Info("JsonGet", "url", url, "duration", time.Since(start))
+	}()
+	resp, err := cli.Get(url)
 	if err != nil {
 		log.Error("JsonGet request error", "url", url, "error", err)
 		return nil, err
